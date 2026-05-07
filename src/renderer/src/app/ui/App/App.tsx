1beb4816659b08d467b7274ae6181a23b6df140b
KaizDA <kochkin@atb.su>
2026-05-07 11:38:02 +0300

    feat: подготовка инфраструктуры для подключения inversify

--- src/renderer/src/app/ui/App/App.tsx
+++ src/renderer/src/app/ui/App/App.tsx
@@ -1,3 +1,25 @@
-export function App(): React.JSX.Element {
-  return <>Текст</>
+import { observer } from 'mobx-react-lite'
+import { makeAutoObservable } from 'mobx'
+
+class Timer {
+  secondsPassed = 0
+
+  constructor() {
+    makeAutoObservable(this, {}, { autoBind: true })
+  }
+
+  increaseTimer(): void {
+    this.secondsPassed += 1
+  }
 }
+
+const myTimer = new Timer()
+
+export const App = observer(() => {
+  return (
+    <div>
+      <div>{myTimer.secondsPassed}</div>
+      <button onClick={myTimer.increaseTimer}>Нажать</button>
+    </div>
+  )
+})
