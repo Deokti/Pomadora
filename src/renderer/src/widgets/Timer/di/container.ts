61a1f95971faed470242487d3d271b76e6b8c65e
KaizDA <kochkin@atb.su>
2026-05-17 21:24:30 +0300

    fix(build): восстановление сборки релиза

--- src/renderer/src/widgets/Timer/di/container.ts
+++
@@ -1,6 +0,0 @@
-import { ContainerModule } from 'inversify'
-import { TimerPresenter } from '../model/TimerPresenter'
-
-export const timerModule = new ContainerModule(({ bind }) => {
-  bind(TimerPresenter).toSelf().inSingletonScope()
-})
--- src/renderer/src/widgets/Timer/index.ts
+++
@@ -1,3 +0,0 @@
-export { Timer } from './ui/Timer'
-export { timerModule } from './di/container'
-export { TimerPresenter } from './model/TimerPresenter'
